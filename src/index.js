9a92347089e73d6941dd5ea2b4537b4a39ece789
Biynazar <bina78@list.ru>
2024-08-27 18:10:43 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+
+let questions = [
+    {
+        id: 1,
+        question: 'Какая версия HTML сегодня актуальна?',
+        answers: [3, 4, 5, 6],
+        trueAnswer: 2 // Индекс 2 соответствует ответу 5
+    },
+    {
+        id: 2,
+        question: 'Какой тег используется для создания ссылки в HTML?',
+        answers: ['<p>', '<a>', '<div>', '<span>'],
+        trueAnswer: 1 // Индекс 1 соответствует ответу <a>
+    },
+    {
+        id: 3,
+        question: 'Какой метод используется для добавления элемента в конец массива в JavaScript?',
+        answers: ['push', 'pop', 'shift', 'unshift'],
+        trueAnswer: 0 // Индекс 0 соответствует ответу push
+    },
+    {
+        id: 4,
+        question: 'Какое свойство CSS отвечает за цвет текста?',
+        answers: ['font-size', 'text-align', 'color', 'background-color'],
+        trueAnswer: 2 // Индекс 2 соответствует ответу color
+    },
+    {
+        id: 5,
+        question: 'Какой символ используется для комментария в JavaScript?',
+        answers: ['//', '/*', '#', '<!--'],
+        trueAnswer: 0 // Индекс 0 соответствует ответу //
+    },
+    {
+        id: 6,
+        question: 'Какой метод используется для преобразования строки в число в JavaScript?',
+        answers: ['toString', 'parseInt', 'parseFloat', 'Number'],
+        trueAnswer: 1 // Индекс 1 соответствует ответу parseInt
+    },
+    {
+        id: 7,
+        question: 'Какая версия CSS используется для создания сеток (Grid)?',
+        answers: [2, 3, 4, 5],
+        trueAnswer: 2 // Индекс 2 соответствует ответу 4
+    },
+    {
+        id: 8,
+        question: 'Какой метод удаляет последний элемент массива в JavaScript?',
+        answers: ['pop', 'push', 'shift', 'unshift'],
+        trueAnswer: 0 // Индекс 0 соответствует ответу pop
+    },
+    {
+        id: 9,
+        question: 'Какой тег используется для создания заголовка первого уровня в HTML?',
+        answers: ['<h1>', '<h2>', '<h3>', '<h4>'],
+        trueAnswer: 0 // Индекс 0 соответствует ответу <h1>
+    },
+    {
+        id: 10,
+        question: 'Какой атрибут используется для добавления альтернативного текста к изображению в HTML?',
+        answers: ['href', 'src', 'alt', 'title'],
+        trueAnswer: 2 // Индекс 2 соответствует ответу alt
+    },
+    {
+        id: 11,
+        question: 'Какой метод преобразует массив в строку в JavaScript?',
+        answers: ['split', 'join', 'concat', 'slice'],
+        trueAnswer: 1 // Индекс 1 соответствует ответу join
+    }
+];
